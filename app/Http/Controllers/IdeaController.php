447ec18342770ebc79fa14dcafee22ae6a7cfeca
Mirzohid <mxudoyberdiyev21@gmail.com>
2025-06-21 16:16:28 +0500

    day 2 | Episode 19 - Check If Idea Voted

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -6,6 +6,7 @@ use App\Models\Idea;
 use App\Http\Requests\StoreIdeaRequest;
 use App\Http\Requests\UpdateIdeaRequest;
 use App\Models\Category;
+use App\Models\Vote;
 
 class IdeaController extends Controller
 {
@@ -16,6 +17,11 @@ class IdeaController extends Controller
     {
         return view('idea.index', [
             'ideas' => Idea::with('user', 'category', 'status')
+                ->addSelect([
+                    'voted_by_user' => Vote::select('id')
+                        ->where('user_id', auth()->id())
+                        ->whereColumn('idea_id', 'ideas.id')
+                ])
                 ->withCount('votes')
                 ->orderBy('id', 'desc')
                 ->simplePaginate(Idea::PAGINATION_COUNT),
--- app/Livewire/IdeaShow.php
+++ app/Livewire/IdeaShow.php
@@ -8,13 +8,14 @@ use Livewire\Component;
 class IdeaShow extends Component
 {
     public $idea;
-
     public $votesCount;
+    public $hasVoted;
 
     public function mount(Idea $idea, $votesCount)
     {
         $this->idea = $idea;
         $this->votesCount = $votesCount;
+        $this->hasVoted = $idea->isVotedByUser(auth()->user());
     }
 
     public function render()
