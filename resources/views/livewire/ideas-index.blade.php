7f51f25128fcb4e8b1f35763cf4975da7556d3c5
Mirzohid <mxudoyberdiyev21@gmail.com>
2025-06-21 22:23:23 +0500

    day 2 | Episode 24 - Status Filters Count

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -34,7 +34,8 @@
         @endforeach
     </div>
     <div class="my-8">
-        {{ $ideas->links() }}
+        {{-- {{ $ideas->links() }} --}}
+        {{ $ideas->appends(request()->query())->links() }}
     </div>
 
 </div>
